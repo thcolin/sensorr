d52fee847215a714ca4540370ddf8c2c3cbb05ae
thcolin <th.colin3@gmail.com>
2019-03-17 19:03:30 +0100

    Fix service headers (Authorization)

--- src/store/sensorr.js
+++ src/store/sensorr.js
@@ -3,7 +3,9 @@ import Sensorr from 'shared/Sensorr'
 const sensorr = new Sensorr(global.config, {
   proxify: true,
   headers: {
-    'Authorization': `Basic ${new Buffer(`${global.config.auth.username}:${global.config.auth.password}`).toString('base64')}`,
+    ...((global.config || { auth: {} }).auth.username ? {
+      'Authorization': `Basic ${new Buffer(`${global.config.auth.username}:${global.config.auth.password}`).toString('base64')}`,
+    } : {}),
   },
 })
 
