4de898c1e1bc7d7eff326196cedbf21b977550a1
thcolin <th.colin3@gmail.com>
2019-10-20 01:51:41 +0200

    0.9.0
    Fix bump script

--- .scripts/bump.sh
+++ .scripts/bump.sh
@@ -5,13 +5,15 @@
 git stash
 # yarn run test
 yarn run build
-yarn --no-git-tag-version version $1
+yarn config set version-git-tag false
+yarn version --new-version $1
 git add -f dist package.json
 git commit -m $1
 git tag v$1
 git push --tags
 git reset --hard HEAD~1
-yarn --no-git-tag-version version $1
+yarn config set version-git-tag false
+yarn version --new-version $1
 git add package.json
 git add package-lock.json
 git commit -m $1
